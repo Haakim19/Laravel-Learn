15d7b428ea2bc9f728e01c94cacee00fd92f5ea8
haakim19 <haakimahamed4@gmail.com>
2025-01-12 21:59:26 +0530

    task -list 02

--- task-list/.history/routes/web_20250108203614.php
+++ task-list/.history/routes/web_20250108203614.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Main Page';
 });
-Route::get('/hello',function() : Returntype {
-
-})
+Route::get('/hello', function (): Returntype {});
