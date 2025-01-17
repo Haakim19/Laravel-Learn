c21b6eb0efc1baae5bffe868a67e3a478cbf3b19
haakim19 <haakimahamed4@gmail.com>
2025-01-18 00:02:48 +0530

    update:User Login(issue:bootstrap\app.php-middware adding)

---
+++ user-login-api/.history/app/Http/Controllers/AuthController_20250117233706.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+    }
+}
---
+++ user-login-api/.history/app/Http/Controllers/AuthController_20250117234254.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->passwod, $user->password)) {
+            return response()->json(['messahe' => 'Invalid credentials'], 401);
+        }
+        $token = $user->createToken('auth_token')->planTextToken;
+    }
+}
