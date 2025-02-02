63a7f549a26a816fe49b0efb3054d3ecdc7e2b53
haakim19 <haakimahamed4@gmail.com>
2025-02-02 23:58:12 +0530

    Blade Code Re-use

---
+++ task-list/.history/resources/views/form.blade_20250202234213.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', isset($task) ? 'Edit'Add Task')
+
+@section('styles')
+    <style>
+        .error-message{
+            color: red;
+            font-size: 0, 8rem;
+        }
+    </style>
+@endsection
+@section('content')
+    <form method="POST" action="{{ route('task.store') }}">
+        {{-- using @csrf for authentication perpose like find out the
+        unaoutotrized web requests --}}
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
+        </div>
+        @error('title')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
+        </div>
+        @error('description')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+
+        <div>
+            <label for="long_description">Long_Description</label>
+            <textarea name="long_description" id="long_description" rows="5">{{ old('long_description')}}</textarea>
+        </div>
+        @error('long_description')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+@endsection
