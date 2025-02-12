49ac07a401169e36f6b204335ef51f445b148de4
haakim19 <haakimahamed4@gmail.com>
2025-02-13 02:04:28 +0530

    project done

---
+++ event-management-api/.history/routes/api_20250212204415.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Login Route
+Route::post('/login', [AuthController::class, 'login']);
+
+// Logout Route
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware(['auth:sanctum']);
+
+// Public Routes
+Route::apiResource('events', EventController::class)
+    ->only(['index', 'show']);
+
+// Protected Routes
+Route::apiResource('events', EventController::class)
+    ->only(['store', 'update', 'destroy'])
+    ->middleware(['auth:sanctum']);
